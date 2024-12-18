2f838d0821e0889292d090fcdc59c39aaf38f6c6
Bainomugisha Libra <148781031+BainoLibra@users.noreply.github.com>
2024-12-18 14:33:36 +0300

    i have updated my project

---
+++ calculator.js
@@ -0,0 +1,8 @@
+const WELCOME_MESSAGE ="Welcome to the simple calculator"
+console.log("Welcome_message", WELCOME_MESSAGE)
+let num1 =10;
+let num2 =5;
+let sum =num1+num2;
+let product =num1*num2;
+console.log("Sum", sum)
+console.log("Product", product)
---
+++ index.js
@@ -0,0 +1,7 @@
+let name ="Libra";
+let age = 26;
+let isStudent = true;
+console.log("Name", name)
+console.log("Age", age)
+console.log("IsStudent", isStudent)
+// console.log('${name} is ${age} and ${isStudent}')
