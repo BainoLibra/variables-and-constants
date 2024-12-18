22e3658dd4d250fa50099f0d3e08e7b31d6e9165
Bainomugisha Libra <148781031+BainoLibra@users.noreply.github.com>
2024-12-18 16:49:49 +0300

    new project was added

---
+++ game.js
@@ -0,0 +1,12 @@
+// Guess the Number Game:
+// Use numbers for the target number.
+// Use booleans to track if the user has guessed correctly.
+// Use strings to provide feedback.
+
+let maxNum =10;
+let guessNum =5;
+if(guessNum === maxNum){
+    console.log("The user guessed right");
+} else{
+    console.log("The user guessed wrongly")
+}
