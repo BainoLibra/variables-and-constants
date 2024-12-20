ee201007e6a9c180454d11f3d4c18b79e520d6b1
Bainomugisha Libra <148781031+BainoLibra@users.noreply.github.com>
2024-12-20 16:18:45 +0300

    i have added new project

---
+++ banking.js
@@ -0,0 +1,11 @@
+// Use objects to represent accounts.
+// Use numbers for balances.
+// Use booleans to check for active accounts.
+let acct1 ={
+    balance: 5000,
+    isActive: true,
+}
+let acct2 ={
+    balance: 10000,
+    isActive: true,
+}
---
+++ variables_and_data_types.js
@@ -0,0 +1,6 @@
+// What is a variable, and how do you declare one in JavaScript?
+let userName ="Libra";
+let age = 26;
+const studentID = "N19";
+let isActive = true;
+console.log(`My name is ${userName}, I am ${age} years old, with unique number ${studentID} and ${isActive}`)
