ea372c535097060216b974878fab11909e4c3cb8
Bainomugisha Libra <148781031+BainoLibra@users.noreply.github.com>
2025-01-03 04:46:22 +0300

    i have made updates

---
+++ null.js
@@ -0,0 +1,7 @@
+let x =null;
+console.log(typeof x);
+let x1 ="null";
+console.log(typeof x1);
+console.log(x==null);
+console.log(x===null);
+console.log(x1===null)
---
+++ substring.js
@@ -0,0 +1,3 @@
+let text ="Libra Baino";
+console.log(text.slice(0, 4));
+console.log(text.substring(0,4));
